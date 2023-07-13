e4402616ac870654a9a7819355b6aedd067214e6
Biel Conde <biel.conde@terramate.io>
2023-07-13 13:19:24 +0200

    Make video as public asset

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div>
     <VitePwaManifest />
     <video controls>
-      <source src="/assets/file_example_MP4.mp4" type="video/mp4" />
+      <source src="/videos/file_example_MP4.mp4" type="video/mp4" />
       Tu navegador no soporta el elemento de video.
     </video>
   </div>
